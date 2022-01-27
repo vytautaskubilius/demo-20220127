b2c01721669ee4d5fca696f4356678ee2bc4320b
Vytautas Kubilius <vytautas.kubilius@gmail.com>
2022-01-27 19:28:49 +0200

    Initial commit.

---
+++ modules/networking/vpc/main.tf
@@ -0,0 +1,7 @@
+module "vpc" {
+  source           = "git@github.com:gruntwork-io/terraform-aws-vpc.git//modules/vpc-app?ref=v0.18.1"
+  aws_region       = "us-east-1"
+  cidr_block       = "10.42.0.0/16"
+  num_nat_gateways = 0
+  vpc_name         = "foo"
+}
---
+++ modules/networking/vpc/terraform.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.67.0"
+    }
+  }
+  required_version = "1.1.1"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
